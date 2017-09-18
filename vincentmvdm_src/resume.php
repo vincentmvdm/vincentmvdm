070826fb8b63be250770d4128885bbce0710f834
Vincent M. van der Meulen <meulen@uw.edu>
2017-09-17 23:53:47 -0700

    Start working on a printable resume

---
+++ vincentmvdm_src/resume.php
@@ -0,0 +1,183 @@
+<?php $title = "Vincent van der Meulen - Resume"; ?>
+
+<?php require("includes/head.php"); ?>
+<?php require("includes/nav.php"); ?>
+
+        <div class="max-width-4 mx-auto pt4 pb4" id="resume-paper">
+            <div class="flex flex-wrap justify-center">
+                <div class="col-12 md-col-5 px2 mt2">
+                    <h1 class="h2 my0">Vincent van der Meulen</h1>
+                    <h2 class="h3 mt1 mb0 regular">Designer and developer</h2>
+                </div>
+                <div class="col-12 md-col-7 px2 mt2">
+                    <ul class="list-style-none pl0 mt0 h5 line-height-4">
+                        <li>
+                            <a href="http://vincentmvdm.com/" target="_blank">http://vincentmvdm.com/</a>
+                        </li>
+                        <li>
+                            <a href="http://github.com/vincentmvdm" target="_blank">http://github.com/vincentmvdm</a>
+                        </li>
+                        <li>meulen@uw.edu</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="flex flex-wrap justify-center">
+                    <div class="col-12 md-col-5 px2 mt1">
+                    <h3 class="my0">Education</h3>
+                    <h4 class="mt2 mb0 line-height-4">University of Washington, Seattle</h4>
+                    <p class="my0">
+                        B.Sc. in Informatics: HCI<br>
+                        Graduating June 2019<br>
+                        3.81/4.00
+                    </p>
+
+                    <h3 class="mt3 mb0">Skills</h3>
+                    <h4 class="mt2 mb0 line-height-4">Product Design</h4>
+                    <p class="mt0">Sketch, Figma, Adobe Creative Suite, Origami, Framer, Flinto, InVision, OmniGraffle</p>
+
+                    <h4 class="mt1 mb0 line-height-4">Development</h4>
+                    <p class="mt0">HTML, Pug, CSS, SCSS, PostCSS, JavaScript, jQuery, React, Redux, Node.js, Go, Python, PHP, Django, SQL, Firebase, Java, Processing, gulp, Git</p>
+
+                    <h3 class="mt3 mb0">Recent Side Projects</h3>
+                    <h4 class="mt2 mb0 line-height-4">react-resume, August 2017</h4>
+
+                    <h4 class="mt2 mb0 line-height-4">Allergenie, June 2017</h4>
+                    <p class="mt0">
+                        Alexa skill that tells people if they can eat certain foods. Node.js, AWS Lambda
+                    </p>
+                    <h4 class="mt2 mb0 line-height-4">caesar, June 2017</h4>
+                    <p class="mt0">
+                        Web app that uses ML to generate playlists that appeal to groups of people. Sketch, React, Redux, Express
+                    </p>
+                </div>
+                <div class="col-12 md-col-7 px2 mt1">
+                    <h3 class="my0">Experience</h3>
+
+                    <h4 class="mt2 mb0 line-height-4">UX Design Intern, Amazon, <span class="regular">Jun. 2017 - Present</span></h4>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h4 class="mt2 mb0 line-height-4">Lead Designer and Developer, DubHacks, <span class="regular">Jan. 2017 - Present</span></h4>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h4 class="mt2 mb0 line-height-4">UX Student Designer, UW-IT, <span class="regular">Jan. 2016 - May 2017</span></h4>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h5 class="mt2">MyUW - University of Washington’s student portal</h5>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h5 class="mt2">Scout - A tool for finding resources on campus</h5>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h4 class="mt2 mb0 line-height-4">Product Designer (Project), Get There, <span class="regular">August 2016</span></h4>
+                    <ul class="mt1 mb0 ml0 pl0">
+                        <li>
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                        <li class="mt1">
+                            <div class="flex">
+                                &bull;
+                                <div class="ml2">
+                                    One line
+                                </div>
+                            </div
+                        </li>
+                    </ul>
+
+                    <h4 class="mt2 mb0 line-height-4">Freelance Graphic Designer, Vincent v/d Meulen, <span class="regular">2014 - 2015</span></h4>
+                    <h4 class="mt0 mb0 line-height-4">Co-Owner and Lead Designer, Creative Aid, <span class="regular">2012 - 2015</span></h4>
+                </div>
+            </div>
+        </div>
+
+<?php require("includes/footer.php"); ?>
