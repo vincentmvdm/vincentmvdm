fce17c557beba252d2e2bd18d8b603e218991e78
Vincent M. van der Meulen <meulen@uw.edu>
2017-09-19 15:26:48 -0700

    Remove fade

--- vincentmvdm/includes/nav.php
+++ vincentmvdm/includes/nav.php
@@ -7,23 +7,23 @@
                     <div class="flex justify-between">
                         <div class="px2">
                             <span id="vincentmvdm">
-                                <a href="index.php" class="fadeBetween sm-hide md-hide lg-hide">Vincent</a>
-                                <a href="index.php" class="fadeBetween xs-hide">Vincent van der Meulen</a>
+                                <a href="index.php" class="sm-hide md-hide lg-hide">Vincent</a>
+                                <a href="index.php" class="xs-hide">Vincent van der Meulen</a>
                             </span>
                         </div>
                         <ul class="flex list-reset">
                             <?php if (basename($_SERVER['PHP_SELF'], '.php') !== 'index') { ?>
                                 <?php if (basename($_SERVER['PHP_SELF'], '.php') === 'about') { ?>
-                                    <a href="index.php#selected-work" class="fadeBetween"><li class="px2">Work</li></a>
+                                    <a href="index.php#selected-work"><li class="px2">Work</li></a>
                                 <?php } else { ?>
-                                    <a href="index.php#selected-work" class="fadeBetween current"><li class="px2">Work</li></a>
+                                    <a href="index.php#selected-work" class="current"><li class="px2">Work</li></a>
                                 <?php } ?>
                             <?php } ?>
 
                             <?php if (basename($_SERVER['PHP_SELF'], '.php') === 'about') { ?>
-                                <a href="about.php" class="fadeBetween current"><li class="px2">About</li></a>
+                                <a href="about.php" class="current"><li class="px2">About</li></a>
                             <?php } else { ?>
-                                <a href="about.php" class="fadeBetween"><li class="px2">About</li></a>
+                                <a href="about.php"><li class="px2">About</li></a>
                             <?php } ?>
 
                             <a href="http://www.downloads.vincentmvdm.com/resume.pdf" target="_blank"><li class="px2">Resume</li></a>
--- vincentmvdm_src/js/index.js
+++ vincentmvdm_src/js/index.js
@@ -1,16 +1,6 @@
 $(function () {
     $('a').fluidbox();
-
-    $('body').fadeTo(800, 1);
-
-    $('.fadeBetween').click(function(event) {
-        event.preventDefault();
-        var dest = this.href;
-        $("body").fadeOut(400, "swing", function() {
-            document.location.href = dest;
-        });
-    });
-
+    
     $(document).scroll(function() {
         if ($('#contents').length) {
             var threshold = $('#contents').offset().top;
