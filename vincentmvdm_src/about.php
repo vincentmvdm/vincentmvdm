a3bd61379c932751758e777067f9425f56349c54
Vincent M. van der Meulen <meulen@uw.edu>
2017-09-19 18:02:06 -0700

    Continue working on the about page

--- vincentmvdm_src/about.php
+++ vincentmvdm_src/about.php
@@ -12,17 +12,10 @@
         <section class="py4">
             <div class="max-width-4 mx-auto">
                 <div class="flex justify-center">
-                    <div class="col-4 px2">
-                        <figure>
-                            <img src="images/about/vincent.jpg" alt="Vincent">
-                            <figcaption>
-                                Hobbies include, but are not limited to, standing outside with my laptop
-                            </figcaption>
-                        </figure>
-                    </div>
-                    <div class="col-8 px2">
+                    <div class="col-12 md-col-9 px2">
+                        <img src="images/about/vincent.jpg" alt="Vincent">
                         <div class="max-width-measure mx-auto">
-                            <h2 class="mt0">About</h2>
+                            <h2 class="mt3">About</h2>
                             <p>
                                 Hi! My name is Vincent and I am a junior studying Informatics, a mix between Design and Computer Science, at the University of Washington in Seattle. I am originally from the Netherlands (you'll notice the funky accent) and am a designer who codes. At the moment, I am really interested in captology, design systems, AR and React.
                             </p>
@@ -46,8 +39,8 @@
                     <h3 class="my0">Bookshelf</h3>
                     <p class="mt1">Books I am currently reading</p>
                 </div>
-                <div class="flex justify-center">
-                    <div class="col-4 px2">
+                <div class="flex flex-wrap justify-center">
+                    <div class="col-12 sm-col-4 px2">
                         <h4>Zero to One</h4>
                         <p class="my0">
                             <em>Notes on Startups, or How to Build the Future</em>
@@ -55,14 +48,14 @@
                             by Peter Thiel and Blake Masters
                         </p>
                     </div>
-                    <div class="col-4 px2">
+                    <div class="col-12 sm-col-4 px2">
                         <h4>Irresistible</h4>
                         <p class="my0">
                             <em>The Rise of Addictive Technology and the Business of Keeping Us Hooked</em>
                         </p>
                         <p class="mt0">by Adam Alter</p>
                     </div>
-                    <div class="col-4 px2">
+                    <div class="col-12 sm-col-4 px2">
                         <h4>The Idealist</h4>
                         <p class="my0">
                             <em>Aaron Swartz and the Rise of Free Culture on the Internet</em>
