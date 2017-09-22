d35b4ef166cf62e5bb773173b7613ad935312555
Vincent M. van der Meulen <meulen@uw.edu>
2017-09-21 19:10:51 -0700

    Add Amazon headers

--- vincentmvdm/about.php
+++ vincentmvdm/about.php
@@ -15,18 +15,18 @@
                             <div class="absolute" id="blue">
                             </div>
                             <img src="images/about/vincent.jpg" alt="Vincent" id="vincent">
-
                         </div>
                         <div class="max-width-measure mx-auto">
+                            <?php $streak = floor((time() - strtotime("2017-05-25")) / (60 * 60 * 24)); ?>
                             <h2 class="mt3">About</h2>
                             <p>
-                                Hi! My name is Vincent and I am a junior studying Informatics, a mix between Design and Computer Science, at the University of Washington in Seattle. I am originally from the Netherlands (you'll notice the funky accent) and am a designer who codes. At the moment, I am really interested in captology, design systems, AR and React.
+                                Hi! My name is Vincent, and I'm a junior studying Informatics, a mix between design and computer science, at the University of Washington in Seattle. I'm originally from the Netherlands (you'll notice the funky accent) and am a designer who codes. At the moment, I'm interested in captology, design systems, AR and React.
                             </p>
                             <p>
-                                As I am learning how design shapes habits, I am trying to create good habits for myself too. I try to write some code every day and open source the majority of it. Today is day 132 of doing that.
+                                As I'm learning how design is used to gain people's attention and influence what they do on a day-to-day basis, I'm trying to use this knowledge to develop habits. Every day, I sit down to write some code and publish it on GitHub. Today is day <?= $streak ?> of doing that.
                             </p>
                             <p>
-                                I have been wanting to visit Boston for forever, so if that's where you live, or you just want to reach out, my email is below ;)
+                                I've wanted to visit Boston for forever, so if that's where you live, or you just want to reach out, my email is below ;)
                             </p>
                             <div class="mt3">
                                 <a href="mailto:meulen@uw.edu?Subject=Hi%20Vincent!" class="btn btn-primary py1 px2">meulen@uw.edu</a>
--- vincentmvdm_src/amazon.php
+++ vincentmvdm_src/amazon.php
@@ -31,19 +31,132 @@
 
                         <h2>Introduction</h2>
                         <p>
-                            I interned at Amazon as a UX designer during the Summer of 2017.
+                            I interned as a UX designer for Amazon Trade-In during the Summer of 2017. Trade-In enables customers to send in their used items and get paid for them.
+                        </p>
+                        <h2>What I did</h2>
+                        <h3>The two projects</h3>
+                        <p>
+                            Over the course of three months, I did two projects. The goal of my first, 3-week project was to improve the Trade-In experience as it exists today. As my second project, which took up the remaining three months, I came up with a new Trade-In feature. This project was unconventional as I was responsible for coming up with the feature (how is it going to make money?) as well as design it. I ultimately accomplished this with the following deliverables:
                         </p>
 
-                        <blockquote>
+                        <ul class="pl0 mt2 list-style-none">
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Competitor Analysis and Literature Review
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Preliminary Recommendations I
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        6 User Interviews
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        User Research Report
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Preliminary Recommendations II
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Sketches
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Wireframes
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Mockups
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Prototypes
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Guerilla tests
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        High fidelity prototype
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Working Backwards document
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="flex">
+                                    &bull;
+                                    <div class="ml2">
+                                        Presentation (2x)
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
 
-                        </blockquote>
-                        <h2>Project 1</h2>
+                        <h3>Side projects</h3>
                         <p>
-                            Pellentesque lobortis ultricies dignissim. Suspendisse pretium velit ac tincidunt scelerisque. Nullam ac accumsan eros. Mauris eget faucibus tellus, sollicitudin luctus urna. In quis placerat purus. Donec varius sem eget cursus commodo. Pellentesque vel ultrices orci. Sed blandit eu massa aliquam varius. Suspendisse dignissim dignissim aliquet. Duis ac augue finibus, lobortis metus ut, volutpat nunc. Sed rutrum, arcu ut blandit pulvinar, turpis leo condimentum ante, id mollis nisi ligula quis nisi. Ut id ante ut urna tristique aliquet. Suspendisse sem lorem, rhoncus a cursus condimentum, volutpat sit amet massa. Pellentesque vel augue in nisl gravida interdum.
+                            Apart from my two main projects, I worked on several things on the side. I participated in an Alexa hackathon and learned how to design voice interfaces. Because my team, consisting out of three engineers, had their hands full with the database, I learned how to use Node.js and wrote the part of our skill that is responsible for making API calls myself. Furthermore, I prototyped part of a new Amazon feature, that makes use of object recognition, in Origami.
                         </p>
-                        <h2>Project 2</h2>
+
+                        <h2>What I Learned</h2>
                         <p>
-                            Pellentesque lobortis ultricies dignissim. Suspendisse pretium velit ac tincidunt scelerisque. Nullam ac accumsan eros. Mauris eget faucibus tellus, sollicitudin luctus urna. In quis placerat purus. Donec varius sem eget cursus commodo. Pellentesque vel ultrices orci. Sed blandit eu massa aliquam varius. Suspendisse dignissim dignissim aliquet. Duis ac augue finibus, lobortis metus ut, volutpat nunc. Sed rutrum, arcu ut blandit pulvinar, turpis leo condimentum ante, id mollis nisi ligula quis nisi. Ut id ante ut urna tristique aliquet. Suspendisse sem lorem, rhoncus a cursus condimentum, volutpat sit amet massa. Pellentesque vel augue in nisl gravida interdum.
+                            During my internship, I improved a lot at in two specific areas: systems thinking and customer obsession. I got better at systems thinking because I was responsible for the business logic at the heart of designs. Junior designers do not frequently get to do this. I had to ensure that my feature can be successful for several years, and that required me to synthesize answers to multiple questions focused on the future: “What are our customers going to want?”, “What is our competition going to do?” and “Where is the Trade-In industry going?”.
+                        </p>
+                        <p>
+                            I became more customer obsessed since I learned that the words we use in our designs are just as important as the pixels. Towards the end of my internship, I spend more time polishing sentences than visuals. I also picked up on something I noticed my colleagues did a lot: making decisions that at first seem counterintuitive from a business perspective to better serve the customer.
                         </p>
                     </div>
                 </div>
