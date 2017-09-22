b2070cb6ec5995ba8f1c85ef226bf0ad86360ffc
Vincent M. van der Meulen <meulen@uw.edu>
2017-09-21 23:01:29 -0700

    Make final changes to index/about

--- vincentmvdm_src/about.php
+++ vincentmvdm_src/about.php
@@ -1,4 +1,4 @@
-<?php $title = "Vincent van der Meulen - Amazon"; ?>
+<?php $title = "About Vincent van der Meulen"; ?>
 
 <?php require("includes/head.php"); ?>
 <?php require("includes/nav.php"); ?>
@@ -12,8 +12,7 @@
                 <div class="flex justify-center">
                     <div class="col-12 md-col-9 px2">
                         <div class="relative" id="about-container">
-                            <div class="absolute" id="blue">
-                            </div>
+                            <div class="absolute" id="blue"></div>
                             <img src="images/about/vincent.jpg" alt="Vincent" id="vincent">
                         </div>
                         <div class="max-width-measure mx-auto">
@@ -23,7 +22,7 @@
                                 Hi! My name is Vincent, and I'm a junior studying Informatics, a mix between design and computer science, at the University of Washington in Seattle. I'm originally from the Netherlands (you'll notice the funky accent) and am a designer who codes. At the moment, I'm interested in captology, design systems, AR and React.
                             </p>
                             <p>
-                                As I'm learning how design is (unfortunately) used to gain people's attention and influence what they do on a day-to-day basis, I'm trying to use this knowledge to develop habits. Every day, I sit down to write some code and publish it on GitHub. Today is day <?= $streak ?> of doing that.
+                                As I'm learning more about what makes people do or not do something, I'm trying to use this knowledge to develop habits. Every day, I sit down to write some code and publish it on GitHub. Today is day <?= $streak ?> of doing that.
                             </p>
                             <p>
                                 I've wanted to visit Boston for forever, so if that's where you live, or you just want to reach out, my email is below ;)
@@ -47,7 +46,6 @@
                         <a href="http://a.co/cM9Bomk" target="_blank">
                             <div class="book-container mt3">
                                 <?php include("includes/book.php"); ?>
-
                                 <div>
                                     <h4 class="mt2">Irresistible: The Rise of Addictive Technology and the Business of Keeping Us Hooked</h4>
                                     <p class="my0">by Adam Alter</p>
@@ -59,7 +57,6 @@
                         <a href="http://a.co/iy4OiE9" target="_blank">
                             <div class="book-container mt3">
                                 <?php include("includes/book.php"); ?>
-
                                 <div>
                                     <h4 class="mt2">Zero to One: Notes on Startups, or How to Build the Future</h4>
                                     <p class="my0">by Peter Thiel and Blake Masters</p>
@@ -71,7 +68,6 @@
                         <a href="http://a.co/h7ScsII" target="_blank">
                             <div class="book-container mt3">
                                 <?php include("includes/book.php"); ?>
-
                                 <div>
                                     <h4 class="mt2">The Idealist: Aaron Swartz and the Rise of Free Culture on the Internet</h4>
                                     <p class="my0">by Justin Peters</p>
