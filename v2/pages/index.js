28aa5c86e07b855d10442b31b3bf25d95dfc1aca
Vincent M. van der Meulen <meulen@uw.edu>
2017-10-09 21:31:08 -0700

    Add heading structure / start styling project section

--- v2/pages/index.js
+++ v2/pages/index.js
@@ -1,27 +1,29 @@
-import { Provider, Heading, Button } from 'rebass';
+import { Provider, Heading, Button, Flex } from 'rebass';
 
 import Card from '../components/Card';
 
 const Index = () => {
     return (
         <Provider>
-            <Heading>
+            <Heading is="h1">
                 I am a designer and developer studying HCI at the University of
                 Washington in Seattle
             </Heading>
-            <Heading>
+            <Heading is="h2">
                 Currently, I am working on DubHacks. Previously, I designed
                 experiences at Amazon.
             </Heading>
             <Heading>Selected Projects</Heading>
             <Button children="meulen@uw.edu" />
             <Button children="Explore my work" />
-            <Card>Amazon Trade-In</Card>
-            <Card>CarMax</Card>
-            <Card>Get There</Card>
-            <Card>Scout</Card>
-            <Card>Perduko</Card>
-            <Card>Creative Aid</Card>
+            <Flex>
+                <Card>Amazon Trade-In</Card>
+                <Card>CarMax</Card>
+                <Card>Get There</Card>
+                <Card>Scout</Card>
+                <Card>Perduko</Card>
+                <Card>Creative Aid</Card>
+            </Flex>
         </Provider>
     );
 };
