617be550ef0f44c7e3fcfdf74085e2a682b554e1
Vincent M. van der Meulen <meulen@uw.edu>
2018-03-12 00:05:24 -0700

    Start on experiments page

---
+++ v1/vincentmvdm/components/experiment.php
@@ -0,0 +1,30 @@
+<?php
+    function experiment($mediaType, $media, $title, $languages, $description) {
+        ?>
+            <section class="py4">
+                <div class="max-width-4 mx-auto">
+                    <div class="col-12 px2">
+                        <div class="max-width-3 mx-auto">
+                            <div class="sixteen-by-nine">
+                                <div>
+                                    <iframe src="https://www.youtube.com/embed/zRl9WaOlYU8" frameborder="0" allowfullscreen=""></iframe>
+                                </div>
+
+                            </div>
+                        </div>
+                        <div class="max-width-measure mx-auto">
+                            <h2>Tandem</h2>
+                            <span class="lighten">Placeholder</span>
+                            <p>
+                                Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                            </p>
+                            <p>
+                                Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        <?php
+    }
+?>
---
+++ v1/vincentmvdm_src/experiments.php
@@ -0,0 +1,100 @@
+<?php $title = "Portfolio Vincent van der Meulen"; ?>
+
+<?php require("includes/head.php"); ?>
+<?php require("includes/nav.php"); ?>
+
+<?php require("components/experiment.php"); ?>
+
+        <section class="py4">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h1 class="mt0">Experiments</h1>
+                        <p>
+                            Curabitur blandit tempus porttitor. Sed posuere consectetur est at lobortis. Donec ullamcorper nulla non metus auctor fringilla.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <?php experiment("placeholder", "placeholder", "placeholder", "placeholder", "placeholder") ?>
+
+        <section class="py4">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h2 class="mt0">Color Me Surprised</h2>
+                        <p>
+                            Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        </p>
+                        <p>
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="py4 bg-section">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h2 class="mt0">Home Sound System</h2>
+                        <p>
+                            Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        </p>
+                        <p>
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="py4">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h2 class="mt0">Similar Details</h2>
+                        <p>
+                            Nullam id dolor id nibh ultricies vehicula ut id elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam id dolor id nibh ultricies vehicula ut id elit. Curabitur blandit tempus porttitor. Nullam id dolor id nibh ultricies vehicula ut id elit. Maecenas sed diam eget risus varius blandit sit amet non magna. Curabitur blandit tempus porttitor.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="py4 bg-section">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h2 class="mt0">Allergenie</h2>
+                        <p>
+                            Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        </p>
+                        <p>
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section class="py4">
+            <div class="max-width-4 mx-auto">
+                <div class="col-12 px2">
+                    <div class="max-width-measure mx-auto">
+                        <h2 class="mt0">Caesar</h2>
+                        <p>
+                            Curabitur blandit tempus porttitor. Donec id elit non mi porta gravida at eget metus. Nulla vitae elit libero, a pharetra augue. Etiam porta sem malesuada magna mollis euismod. Cras justo odio, dapibus ac facilisis in, egestas eget quam.
+                        </p>
+                        <p>
+                            Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nulla vitae elit libero, a pharetra augue. Nulla vitae elit libero, a pharetra augue. Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+<?php require("includes/footer.php"); ?>
